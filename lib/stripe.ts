3ae97fa76cdf8ae502367d4fa47879ed90a636f3
Sumit Saha <158603168+sumit372717@users.noreply.github.com>
2026-09-08 21:14:34 +0600

    Delete lib directory

--- lib/stripe.ts
+++
@@ -1,9 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js'
-
-export const getStripe = () => {
-  const publishableKey = process.env.NEXTPUBLICSTRIPEPUBLISHABLEKEY
-  if (!publishableKey) {
-    throw new Error('Stripe publishable key is not set')
-  }
-  return loadStripe(publishableKey)
-}
