28298b8be5c698e5341b4c68448879aaed423495
Sumit Saha <158603168+sumit372717@users.noreply.github.com>
2026-09-12 00:44:42 +0600

    Add files via upload

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import { loadStripe } from '@stripe/stripe-js'
 
 export const getStripe = () => {
-  const publishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+  const publishableKey = process.env.NEXTPUBLICSTRIPEPUBLISHABLEKEY
   if (!publishableKey) {
     throw new Error('Stripe publishable key is not set')
   }
